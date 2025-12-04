84d1b108523b55904d1a67bc86870d253ba97061
Sadai Lazca <zuribonilla234@gmail.com>
2025-12-04 02:48:50 +0000

    se agregaron opciones

--- Metodo_simplex.java
+++ Metodo_simplex.java
@@ -2,5 +2,12 @@ import java.util.Scanner;
 public class Metodo_simplex{
     public static void main(String[]args){
         Scanner lector = new Scanner(System.in);
+
+        int variables,restricciones;
+        System.out.println("Metodo simplex");
+        System.out.println("Elige una de las siguiente opciones: ");
+        System.out.println("1. Maximizar");
+        System.out.println("2. Minimizar");
+        int opcion = lector.nextInt();
     }
 }
